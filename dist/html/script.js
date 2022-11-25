4bed6b96ab95c775cac550812173586ef02020bc
Teemu Pölkki <temesus@hotmail.com>
2022-11-25 17:35:01 +0200

    modified the list of organizers

--- dist/html/script.js
+++ dist/html/script.js
@@ -1,5 +1,9 @@
 /*
-Sarjajärjestäjäspesifit kirjoitusasut
+*Sarjajärjestäjäspesifit kirjoitusasut
+* Tämä kammottava koodi on täällä syystä
+* Järjestäjätieto tulee Google Calendarista "kutsut" kohdasta
+* Kutsua voi vain sähköpostiosoitteen
+* Buildiskripti splittaa ennen @ merkkiä olevan osan ja palauttaa sen "järjestäjä" kohtaan
 */
 const elements = document.querySelectorAll("td[label='Järjestäjä:']");
 elements.forEach(element => {
@@ -10,17 +14,30 @@ elements.forEach(element => {
     case 'fisra':
       element.innerHTML = '<a href="https://simracing.fi/fisra">FiSRA</a>'
       break;
-    case 'fisu':
+    case 'fisra_gt_challenge':
+      element.innerHTML = '<a href="https://simracing.fi/fisra">FiSRA</a>'
+      break;
+    case 'finnish_simracing_united':
       element.innerHTML = '<a href="https://simracing.fi/sarjat/11">FiSU</a>'
+      break;
+    case 'fisu':
+      element.innerHTML = '<a href="https://simracing.fi/sarjat/11">Finnish Simracing United</a>'
       break;
     case 'trellet':
       element.innerHTML = '<a href="https://trellet.net/">Trellet</a>'
       break;
     case 'f.a.s.t':
       element.innerHTML = '<a href="https://fast-simracing.com/">F.A.S.T.</a>'
+      break;
+    case 'fast':
+      element.innerHTML = '<a href="https://fast-simracing.com/">F.A.S.T.</a>'
+      break;
+    case 'prcf_esports':
+      element.innerHTML = '<a href="http://www.porsche-racing-club-finland.fi/PorscheClubs/prc_finland/pc_main.nsf/web/FCAB2C256B83FE18C125847B003B3544">PRCF Esports</a>'
+      break;
     default:
       element.innerHTML = element.innerHTML
-
+      break;
   }
 })
 //filtterit 
