ad0406827a9015a8da64547988c21bdb3a03ac6c
Teemu Pölkki <temesus@hotmail.com>
2022-11-25 17:39:32 +0200

    simplified the code using the api nrde provided, thanks!

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -4,3 +4,45 @@ exports.padZero = function (string) {
   }
   else return string
 }
+
+exports.simNameNormalizer = function (simulator) {
+  switch (simulator.toLowerCase()) {
+    //Assetto Corsa
+    case "assetto corsa":
+      return "AC";
+    case "assettocorsa":
+      return "AC";
+    case "assetto-corsa":
+      return "AC";
+    //Assetto Corsa Competizione
+    case "assetto corsa competizione":
+      return "ACC";
+    case "assettocorsa competizione":
+      return "ACC";
+    case "assetto-corsa-competizione":
+      return "ACC";
+    //Automobilista
+    case "ams":
+      return "Automobilista";
+    //Automobilista 2
+    case "ams2":
+      return "Automobilista 2";
+    case "ams 2":
+      return "Automobilista 2";
+    case "automobilista2":
+      return "Automobilista 2";
+    // Dirt Rally 2.0
+    case "dr2":
+      return "Dirt Rally 2.0";
+    case "dirtrally2":
+      return "Dirt Rally 2.0";
+    case "dirtrally2.0":
+      return "Dirt Rally 2.0";
+    case "dirt rally 2":
+      return "Dirt Rally 2.0";
+    default:
+      return simulator;
+  }
+}
+
+console.log(this.simNameNormalizer("assetto corsa"))
