c2d7227b81434393e94791ac982de41e68e328bb
Teemu Pölkki <temesus@hotmail.com>
2022-12-13 19:48:26 +0200

    added gt7 as per simracingfi api

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,46 +1,47 @@
-exports.padZero = function (string) {
+exports.padZero = string => {
   if (string.length < 2) {
-    return `0${string}`
-  }
-  else return string
-}
+    return `0${string}`;
+  } else return string;
+};
 
-exports.simNameNormalizer = function (simulator) {
+exports.simNameNormalizer = simulator => {
   switch (simulator.toLowerCase()) {
     //Assetto Corsa
-    case "assetto corsa":
-      return "AC";
-    case "assettocorsa":
-      return "AC";
-    case "assetto-corsa":
-      return "AC";
+    case 'assetto corsa':
+      return 'AC';
+    case 'assettocorsa':
+      return 'AC';
+    case 'assetto-corsa':
+      return 'AC';
     //Assetto Corsa Competizione
-    case "assetto corsa competizione":
-      return "ACC";
-    case "assettocorsa competizione":
-      return "ACC";
-    case "assetto-corsa-competizione":
-      return "ACC";
+    case 'assetto corsa competizione':
+      return 'ACC';
+    case 'assettocorsa competizione':
+      return 'ACC';
+    case 'assetto-corsa-competizione':
+      return 'ACC';
     //Automobilista
-    case "ams":
-      return "Automobilista";
+    case 'ams':
+      return 'Automobilista';
     //Automobilista 2
-    case "ams2":
-      return "Automobilista 2";
-    case "ams 2":
-      return "Automobilista 2";
-    case "automobilista2":
-      return "Automobilista 2";
+    case 'ams2':
+      return 'Automobilista 2';
+    case 'ams 2':
+      return 'Automobilista 2';
+    case 'automobilista2':
+      return 'Automobilista 2';
     // Dirt Rally 2.0
-    case "dr2":
-      return "Dirt Rally 2.0";
-    case "dirtrally2":
-      return "Dirt Rally 2.0";
-    case "dirtrally2.0":
-      return "Dirt Rally 2.0";
-    case "dirt rally 2":
-      return "Dirt Rally 2.0";
+    case 'dr2':
+      return 'Dirt Rally 2.0';
+    case 'dirtrally2':
+      return 'Dirt Rally 2.0';
+    case 'dirtrally2.0':
+      return 'Dirt Rally 2.0';
+    case 'dirt rally 2':
+      return 'Dirt Rally 2.0';
+    case 'gran turismo 7 (ps)':
+      return 'Gran Turismo 7';
     default:
       return simulator;
   }
-}
+};
